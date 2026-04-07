69bc3eb3f8051592d402d26df183ba718042b93b
cczzHoward <160112481+cczzHoward@users.noreply.github.com>
2026-04-07 13:01:18 +0800

    Add: coin-change - submission-2

---
+++ Data Structures & Algorithms/coin-change/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # dp => [0 ... amount]
+        # amount + 1 是因為有多一個 "0"
+
+        dp = [amount+1] * (amount+1)
+        # 0 元可以用 0 個硬幣達成
+        dp[0] = 0
+        
+        # 每一輪都填寫 dp[a]
+        for a in range(1, amount + 1):
+            for c in coins:
+                if a - c >= 0:
+                    # example a=7, c=4
+                    # dp[7] = min(dp[7], 1 + dp[3])
+                    #                    1個四元 + 達成三元的硬幣數
+                    dp[a] = min(dp[a], 1 + dp[a-c])
+
+        if dp[amount] != amount+1:
+            return dp[amount]
+        else:
+            return -1
