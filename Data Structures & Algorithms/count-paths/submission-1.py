e32880ad176a5959fa29c7c8e9a0a73e6725ca34
cczzHoward <160112481+cczzHoward@users.noreply.github.com>
2026-04-09 21:08:53 +0800

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        row = [1] * n
+
+        # 最底下那一排全部都是一 所以不用跑
+        # 從第二排開始跑就好 所以只要跑 m-1 徘 (m-1 次迴圈)
+        for i in range(m-1):
+            newRow = [1] * n
+            # 最右邊的數字 (n-1) 一定是1
+            # 所以從右邊數來第二個數字 (n-2) 開始跑
+            for j in range(n - 2, -1, -1):
+                # 現在這個格子有的 unique path 的數量 => 右邊格子 + 下面格子
+                newRow[j] = newRow[j+1] + row[j]
+            row = newRow
+        
+        return row[0]
