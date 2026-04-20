f8be2eef02197ab35df0fff296ada04b7c42ec35
cczzHoward <160112481+cczzHoward@users.noreply.github.com>
2026-04-20 13:14:03 +0800

    Add: rotting-fruit - submission-1

---
+++ Data Structures & Algorithms/rotting-fruit/submission-1.py
@@ -0,0 +1,37 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        q = deque()
+        time, fresh = 0, 0
+
+        ROWS, COLS = len(grid), len(grid[0])
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    fresh += 1
+                if grid[r][c] == 2:
+                    q.append([r, c])
+        
+        def addOrange(r, c):
+            # 要改外部變數要宣告
+            nonlocal fresh
+            if (r not in range(ROWS) or
+                c not in range(COLS) or
+                grid[r][c] != 1):
+                return
+            
+            grid[r][c] = 2
+            q.append([r, c])
+            fresh -= 1
+
+        while q and fresh > 0:
+            for i in range(len(q)):
+                r, c = q.popleft()
+
+                addOrange(r+1, c)
+                addOrange(r-1, c)
+                addOrange(r, c+1)
+                addOrange(r, c-1)
+            
+            time += 1
+
+        return time if fresh == 0 else -1
