e806caf053bdffb336037e28e16f376d40258905
cczzHoward <160112481+cczzHoward@users.noreply.github.com>
2026-04-22 12:51:46 +0800

    Add: surrounded-regions - submission-1

---
+++ Data Structures & Algorithms/surrounded-regions/submission-1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        ROWS, COLS  = len(board), len(board[0])
+
+        def capture(r, c):
+            if (r not in range(ROWS) or
+                c not in range(COLS) or
+                board[r][c] != 'O'):
+                return
+            
+            board[r][c] = 'T'
+            capture(r+1, c)
+            capture(r-1, c)
+            capture(r, c+1)
+            capture(r, c-1)
+        
+        # 1. Capture unsurrounded regions (O -> T)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (board[r][c] == 'O' and 
+                    (r in [0, ROWS-1] or c in [0, COLS-1])):
+                    capture(r, c)
+
+        # 2 & 3
+        # O -> X 再 T -> O
+        # 這邊要照順序執行 如果反過來會出問題
+        for r in range(ROWS):
+            for c in range(COLS):
+                # 2. Capture surrounded regions (O -> X)
+                if board[r][c] == 'O':
+                    board[r][c] = 'X'
+                # 3. Uncapture unsurrounded regions (T -> O)
+                elif board[r][c] == 'T':
+                    board[r][c] = 'O'
