15ed47c00ef2c51aa9697bfcfa4a5f0e48843698
cczzHoward <160112481+cczzHoward@users.noreply.github.com>
2026-04-29 12:57:04 +0800

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        # maxHeap
+        stones = [-s for s in stones]
+        heapq.heapify(stones)
+
+        # 有兩個石頭以上(含)就要碰撞
+        while len(stones) > 1:
+            first = heapq.heappop(stones)
+            second = heapq.heappop(stones)
+            # 第一顆石頭一定 >= 第二顆石頭
+            if first < second:
+                heapq.heappush(stones, first -second)        
+        
+        # 回傳前塞一個零
+        # 原本 stones 是空的 => return 0
+        # 原本 stones 有一值 => return value(因為 value 一定比 0 小)
+        stones.append(0)
+        return abs(stones[0])
