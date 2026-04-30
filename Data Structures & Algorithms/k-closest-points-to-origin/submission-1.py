f1e95582c8e5e3979db06d36c6deb11efffd1256
cczzHoward <160112481+cczzHoward@users.noreply.github.com>
2026-04-30 12:55:02 +0800

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        # 今天是 heap 的第三題 也是第一題 normal
+        # 居然比前兩題 easy 簡單, 我震驚了
+        
+        minHeap = []
+        # linear time
+        for x, y in points:
+            # 我們不需要真的求他的距離, 我們只求最近的點
+            # dist 已經符合這個需求了 不需要再多開根號 (除非他有要求回傳真的距離)
+            dist = (x**2) + (y**2)
+            minHeap.append([dist, x, y])
+        
+        # 轉成 minHeap
+        heapq.heapify(minHeap)
+        # 拿 k 個出來 (取出 k 個最近的 point)
+        res = []
+        while k > 0:
+            dist, x, y = heapq.heappop(minHeap)
+            res.append([x, y])
+            k -= 1
+        return res
