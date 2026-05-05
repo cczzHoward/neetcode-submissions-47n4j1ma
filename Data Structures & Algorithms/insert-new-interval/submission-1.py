b9686f464111cf5d35564302a5d84728819ebe0f
cczzHoward <160112481+cczzHoward@users.noreply.github.com>
2026-05-05 21:01:06 +0800

    Add: insert-new-interval - submission-1

---
+++ Data Structures & Algorithms/insert-new-interval/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+        for i in range(len(intervals)):
+            # 如果插入的線的尾巴比 i 線的開頭小 -> 較小且沒連結 -> 把 newInterval 丟到 res 
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            # 如果插入的線的開頭比 i 線的尾巴大 -> 較大且沒連結 -> 把 intervals[i] 丟到 res
+            elif newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
+            
+            # 上面兩個是沒相交的情況
+            # else 則是有相交的情況 -> merge 
+            else:
+                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+        
+        # newInterval 還沒有被加進去 res -> 要記得加入
+        res.append(newInterval)
+        return res
+        
