6c39fc245b437d6375dfc66a724f825137954c7d
cczzHoward <160112481+cczzHoward@users.noreply.github.com>
2026-07-14 21:20:23 +0800

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,32 @@
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        # ["Hello", "everyone"] -> "5#Hello8#everyone"
+        encode_str = ""
+
+        for string in strs:
+            encode_str += str(len(string)) + '#' + string
+        
+        return encode_str
+
+    def decode(self, s: str) -> List[str]:
+        # "5#Hello8#everyone" -> ["Hello", "everyone"]
+
+        decode_list = []
+
+        i = 0
+        while i < len(s):
+            # 1. 找現在這個字串有多長
+            j = i
+            while s[j] != '#':
+                j += 1
+            length = int(s[i:j])
+
+            # 2. 把字串加進 decode_list
+            i = j + 1
+            j = i + length
+            decode_list.append(s[i:j])
+
+            # 3. 找下一個字串
+            i = j
+
+        return decode_list
