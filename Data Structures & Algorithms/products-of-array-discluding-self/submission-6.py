29382fcb92bf67dbefa0fcfa02a21c7f97515966
cczzHoward <160112481+cczzHoward@users.noreply.github.com>
2026-07-15 18:54:32 +0800

    Add: products-of-array-discluding-self - submission-6

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-6.py
@@ -0,0 +1,37 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        """
+        nums   : [ 1,  2,  3,  4]
+        prefix : [ 1,  2,  6, 24]
+        postfix: [24, 24, 12,  4]
+
+        res    : [24, 12,  8,  6]
+        res[1] = prefix[0]   * postfix[2]
+        res[2] = prefix[1]   * postfix[3]
+        res[i] = prefix[i-1] * postfix[i+1]
+
+        
+        prefix : [           1,                    2,                    6,          24]
+        postfix: [          24,                   24,                   12,           4]
+        res    : [1*postfix[1], prefix[0]*postfix[2], prefix[1]*postfix[3], prefix[2]*1]
+        res    : [          24,                   12,                    8,           6]
+        
+        兩個迴圈, 一個在 res 放 prefix, 一個在 res 放 postfix, 然後相乘載一起 return
+        """
+
+        ans = [1] * len(nums)
+
+        # prefix part
+        prefix = 1
+        for i in range(len(nums)-1):
+            prefix *= nums[i]
+            ans[i+1] *= prefix
+
+        # postfix part
+        postfix = 1
+        for i in range(len(nums)-1, 0, -1):
+            postfix *= nums[i]
+            ans[i-1] *= postfix
+        
+        return ans
+
