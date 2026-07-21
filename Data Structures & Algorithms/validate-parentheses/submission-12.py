a79b2c768b2eb80d360f9217e9ddea44bcd909a7
cczzHoward <160112481+cczzHoward@users.noreply.github.com>
2026-07-21 21:43:30 +0800

    Add: validate-parentheses - submission-12

---
+++ Data Structures & Algorithms/validate-parentheses/submission-12.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        valid_dict = {'}':'{', ']':'[', ')':'('}
+        stack = []
+
+        for string in s:
+            # case )]}
+            if string in valid_dict:
+                if stack:
+                    cur_element = stack.pop()
+                else:
+                    cur_element = '#'
+                
+                if cur_element != valid_dict[string]:
+                    return False
+
+            # case ([{
+            else:
+                stack.append(string)
+        
+        # if stack still have other element haven't been match -> unvalid
+        return not stack
